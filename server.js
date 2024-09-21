85a6f7bc855bc081450ecae5dbd297c0d4d24670
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2024-09-21 13:46:39 +0800

    Created basic crud api as well as Dockerfile

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const pool = require("./db");
+const port = 1337;
+
+const app = express();
+
+app.use(express.json());
+
+// Routes
+
+app.get("/", async (req, res) => {
+  try {
+    const data = await pool.query("SELECT * FROM schools");
+    res.status(200).json({ children: data.rows });
+  } catch (error) {
+    res.status(200).json({ message: "There was an error please try again" });
+  }
+});
+
+app.post("/", async (req, res) => {
+  const { name, location } = req.body;
+  const data = await pool.query(
+    "INSERT INTO schools (name, address) VALUES ($1, $2)",
+    [name, location]
+  );
+  res.status(200).send({ message: "Successfully created record!" });
+  try {
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+  }
+
+  res.status(200).json({
+    message: `Your name: ${name}, your location: ${location}`,
+  });
+});
+
+app.get("/setup", async (req, res) => {
+  try {
+    await pool.query(
+      "CREATE TABLE schools(id SERIAL PRIMARY KEY, name VARCHAR(100), address VARCHAR(100)"
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+  }
+});
+
+app.listen(port, () => console.log(`Server started on ${port}`));
